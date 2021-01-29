f5076fdc64df23ecf6c127500c8c2afe1f778d11
Germán <germanmacchia@gmail.com>
2021-01-28 22:41:50 -0300

    reactivo

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -94,7 +94,8 @@ Si utilizo el arrow function me ahorro de bindearlo.
 							}
 								
 
-							<form className="mid-form" onSubmit={this.recibirFormulario}>
+							<form className="mid-form" onSubmit={this.recibirFormulario} onChange={this.recibirFormulario}>
+							{/* On submit lo hace cuando se manda. onChangelocambia de forma inmediata y reactiva*/}
 								<div className="form-group">
 									<label htmlFor="nombre">Nombre</label>
 									<input type="text" name="nombre" ref={this.nombreRef} />
